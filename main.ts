ea40b049120d8d3833daecc69084125ad8ccfbe8
toshinori.sato <tyahhahha@gmail.com>
2025-09-11 23:51:03 +0900

    solve a02

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
-const n = Number(input);
-console.log(n * n);
+const [line1, line2] = input.split("\n");
+const [_, x] = line1!.split(" ");
+const as = line2?.split(" ");
+console.log(as?.includes(x!) ? "Yes" : "No");
