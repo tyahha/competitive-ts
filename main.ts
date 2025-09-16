50306850dd91d0fb9d6feea3e81bfa8771bbe673
toshinori.sato <tyahhahha@gmail.com>
2025-09-16 22:47:32 +0900

    solve b03

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
-const [line1, line2, line3] = input.split("\n");
-const [_, n] = line1!.split(" ").map(Number);
+const [_, line2] = input.split("\n");
 const ps = line2!.split(" ").map(Number);
-const qs = line3!.split(" ").map(Number);
 let found = false;
-for (const p of ps) {
-  for (const q of qs) {
-    if (p + q == n) {
-      found = true;
-      break;
+for (let i = 0; i < ps.length - 2; i++) {
+  for (let j = i + 1; j < ps.length - 1; j++) {
+    for (let k = i + 2; k < ps.length; k++) {
+      if (ps[i]! + ps[j]! + ps[k]! === 1000) {
+        found = true;
+        break;
+      }
     }
+    if (found) break;
   }
+  if (found) break;
 }
 console.log(found ? "Yes" : "No");
