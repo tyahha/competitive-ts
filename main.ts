19e124c451db1a306c15597ef88ca60153b92354
toshinori.sato <tyahhahha@gmail.com>
2025-09-19 23:46:59 +0900

    solve a005

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
-let n = 0;
-input
-  .trim()
-  .split("")
-  .reverse()
-  .forEach((c, i) => {
-    n += Math.pow(2, i) * (c === "1" ? 1 : 0);
-  });
-
-console.log(n);
+const [n, k] = input.split(" ").map(Number);
+let count = 0;
+for (let i = 1; i <= n!; i++) {
+  for (let j = 1; j <= n!; j++) {
+    if (i + j < k! && k! - (i + j) <= n!) {
+      count++;
+    }
+  }
+}
+console.log(count);
