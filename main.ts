79346e97176399b7f1a8b724b2121b5d1ff0d870
toshinori.sato <tyahhahha@gmail.com>
2025-09-20 22:36:19 +0900

    solve06

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
-const [n, k] = input.split(" ").map(Number);
-let count = 0;
-for (let i = 1; i <= n!; i++) {
-  for (let j = 1; j <= n!; j++) {
-    if (i + j < k! && k! - (i + j) <= n!) {
-      count++;
-    }
-  }
-}
-console.log(count);
+const [_, line2, ...lrs] = input.split("\n");
+const as = line2!.split(" ").map(Number);
+const [sums, __] = as.reduce<[number[], number]>(
+  ([acc, sum], a) => {
+    acc.push(sum + a);
+    return [acc, sum + a];
+  },
+  [[], 0]
+);
+const n = as.length;
+lrs.forEach((lr) => {
+  const [l, r] = lr.split(" ").map(Number);
+  if (!l || !r) return;
+  console.log(sums[r! - 1]! - (sums[l! - 2] || 0));
+});
