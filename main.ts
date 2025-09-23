48ca6a11432ed4fe79a128023ebf94eebebe4d4b
toshinori.sato <tyahhahha@gmail.com>
2025-09-23 23:17:23 +0900

    solve a07

--- main.ts
+++ main.ts
@@ -1,27 +1,17 @@
 import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
-const [_, line2, ...lrs] = input.split("\n");
-const as = line2!.split(" ").map(Number);
-const [sums, __] = as.reduce<[[number, number][], [number, number]]>(
-  ([acc, [wins, looses]], a) => {
-    if (a === 1) {
-      wins++;
-    } else {
-      looses++;
-    }
-    acc.push([wins, looses]);
-    return [acc, [wins, looses]];
-  },
-  [[[0, 0]], [0, 0]]
-);
-const n = as.length;
-lrs.forEach((lr) => {
-  const [l, r] = lr.split(" ").map(Number);
-  if (!l || !r) return;
-  const [pw, pl] = sums[r!]!;
-  const [qw, ql] = sums[l - 1]!;
-  const wins = pw - qw;
-  const looses = pl - ql;
-  console.log(wins > looses ? "win" : wins < looses ? "lose" : "draw");
-});
+const [line1, line2, ...lrs] = input.split("\n");
+const d = Number(line1);
+const n = Number(line2);
+const aa = Array(d + 1).fill(0);
+for (let i = 0; i < n; i++) {
+  const [l, r] = lrs![i]!.split(" ").map(Number);
+  aa[l! - 1]++;
+  aa[r!]--;
+}
+let count = 0;
+for (let i = 0; i < d; i++) {
+  count += aa[i]!;
+  console.log(count);
+}
