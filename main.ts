874bad6f41afc72fd0d2ac857c8f0be2f584a744
toshinori.sato <tyahhahha@gmail.com>
2025-09-24 22:27:33 +0900

    solve b07

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("/dev/stdin", "utf-8");
 const [line1, line2, ...lrs] = input.split("\n");
-const d = Number(line1);
+const t = Number(line1);
 const n = Number(line2);
-const aa = Array(d + 1).fill(0);
+const aa = Array(t).fill(0);
 for (let i = 0; i < n; i++) {
   const [l, r] = lrs![i]!.split(" ").map(Number);
-  aa[l! - 1]++;
+  aa[l!]++;
   aa[r!]--;
 }
 let count = 0;
-for (let i = 0; i < d; i++) {
+for (let i = 0; i < t; i++) {
   count += aa[i]!;
   console.log(count);
 }
