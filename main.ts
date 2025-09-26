454b626f884415201ed7673b509e7fb4a2e5e11d
toshinori.sato <tyahhahha@gmail.com>
2025-09-26 22:07:19 +0900

    solve b08

--- main.ts
+++ main.ts
@@ -7,31 +7,33 @@ const getLine = (() => {
   return () => lines[idx++];
 })();
 
-const [h, w, n] = getLine().split(" ").map(Number);
-const m: number[][] = Array(h + 1)
+const MAX = 1500;
+const n = Number(getLine());
+const m: number[][] = Array(MAX + 2)
   .fill(0)
-  .map(() => Array(w + 1).fill(0));
-
+  .map(() => Array(MAX + 2).fill(0));
 for (let i = 0; i < n; i++) {
+  const [x, y] = getLine().split(" ").map(Number);
+  m[y][x]++;
+}
+for (let i = 1; i <= MAX; i++) {
+  for (let j = 1; j <= MAX; j++) {
+    const prev = m[i][j - 1];
+    m[i][j] += prev;
+  }
+}
+for (let j = 1; j <= MAX; j++) {
+  for (let i = 1; i <= MAX; i++) {
+    const prev = m[i - 1][j];
+    m[i][j] += prev;
+  }
+}
+const q = Number(getLine());
+for (let i = 0; i < q; i++) {
   const [a, b, c, d] = getLine().split(" ").map(Number);
-  m[a - 1][b - 1] += 1;
-  m[c][d] += 1;
-  m[a - 1][d] -= 1;
-  m[c][b - 1] -= 1;
-}
-
-for (let i = 0; i < h; i++) {
-  for (let j = 0; j < w; j++) {
-    m[i][j + 1] += m[i][j];
-  }
-}
-
-for (let j = 0; j < w; j++) {
-  for (let i = 0; i < h; i++) {
-    m[i + 1][j] += m[i][j];
-  }
-}
-
-for (let i = 0; i < h; i++) {
-  console.log(m[i].slice(0, w).join(" "));
+  const total = m[d][c];
+  const left = m[d][a - 1];
+  const up = m[b - 1][c];
+  const corner = m[b - 1][a - 1];
+  console.log(total - left - up + corner);
 }
